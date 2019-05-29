fee2575e14a1723966d840e7ed5b5d10ab4e13bf
AidaRos95 <aidajros@gmail.com>
2019-05-29 14:27:20 +0200

    Connection established, items table created

---
+++ db.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg')
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://postgres:secret@localhost:5432/postgres'
+const pool = new Pool({ connectionString: connectionString })
+
+pool.on('error', (err, client) => {
+  console.error('error event on pool', err)
+})
+
+pool.query(`
+  CREATE TABLE IF NOT EXISTS items
+    (
+      id SERIAL,
+      title VARCHAR(255),
+      picture_url VARCHAR,
+      price DECIMAL(6, 2),
+      description VARCHAR(350),
+      sellerid INTEGER
+    );
+`)
+  .catch(console.err)
+
