09febcf6c8080508959865765208cd0664790555
AidaRos95 <aidajros@gmail.com>
2019-05-29 17:14:45 +0200

    Added foreign key

--- index.js
+++ index.js
@@ -3,15 +3,11 @@ const itemsRouter = require('./items/routes')
 const sellersRouter = require('./sellers/routes')
 
 const app = express()
-
 const port = process.env.PORT || 4000
 
 app
   .use(itemsRouter)
   .use(sellersRouter)
-
-app.listen(port, () => console.log(`Listening on port ${port}`))
-
-
+  .listen(port, () => console.log(`Listening on port ${port}`))
 
 module.exports = app
